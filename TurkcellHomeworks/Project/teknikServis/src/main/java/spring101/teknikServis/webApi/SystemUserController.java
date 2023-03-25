dd5ffc81eea76e22b9a9c79a70a6deeaeaa3be8d
Bahadır Sami Ekmen <91840099+samimisami@users.noreply.github.com>
2023-03-25 12:42:40 +0300

    security config and bug fix

--- TurkcellHomeworks/Project/teknikServis/src/main/java/spring101/teknikServis/webApi/SystemUserController.java
+++ TurkcellHomeworks/Project/teknikServis/src/main/java/spring101/teknikServis/webApi/SystemUserController.java
@@ -1,13 +1,14 @@
 package spring101.teknikServis.webApi;
 
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
-import spring101.teknikServis.business.abstracts.SystemUserService;
+import spring101.teknikServis.business.concretes.SystemUserManager;
 import spring101.teknikServis.webApi.dto.SystemUserDto;
 
 @RestController
@@ -17,13 +18,14 @@ import spring101.teknikServis.webApi.dto.SystemUserDto;
 @AllArgsConstructor
 public class SystemUserController {
 	
-	private SystemUserService systemUserService;
+	private SystemUserManager systemUserManager;
 	
-	public String addUserAccount(@ModelAttribute("user") SystemUserDto systemUserDto){
+	@RequestMapping(value="/add",method=RequestMethod.POST,consumes="application/json")
+	public String addUserAccount(@RequestBody SystemUserDto systemUserDto){
 		
-		systemUserService.save(systemUserDto);
+		systemUserManager.save(systemUserDto);
 		
 		return "redirect:/registration?success";
 	}
-	 
+	
 }
